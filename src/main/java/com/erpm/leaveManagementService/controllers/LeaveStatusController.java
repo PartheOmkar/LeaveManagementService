239f5eb1388360b458ed054edfa1beddff47ef2f
Omkar Parthe <partheomkar362@gmail.com>
2023-12-03 10:57:07 +0530

    required operations for leave status APIs added

--- src/main/java/com/erpm/leaveManagementService/controllers/LeaveStatusController.java
+++ src/main/java/com/erpm/leaveManagementService/controllers/LeaveStatusController.java
@@ -1,15 +1,48 @@
 package com.erpm.leaveManagementService.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erpm.leaveManagementService.entitys.LeaveStatus;
 import com.erpm.leaveManagementService.services.LeaveStatusService;
 
 @RestController
+@RequestMapping("/leaveStatus")
 public class LeaveStatusController {
 
 	@Autowired
 	LeaveStatusService leaveStatusService;
 	
+	@PostMapping
+	public ResponseEntity<LeaveStatus> addLeaveStatus(@RequestBody LeaveStatus leaveStatus) {
+		LeaveStatus savedLeaveStatus = leaveStatusService.addLeaveStatus(leaveStatus);
+		return ResponseEntity.ok(savedLeaveStatus);
+	}
 	
+	@GetMapping("/{leaveStatusId}")
+	public ResponseEntity<LeaveStatus> getLeaveStatusById(@PathVariable int leaveStatusId){
+		LeaveStatus leaveStatus = leaveStatusService.getLeaveStatusById(leaveStatusId);
+		return ResponseEntity.ok(leaveStatus);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LeaveStatus>> getAllLeaveStatus(){
+		List<LeaveStatus> leaveStatus = leaveStatusService.getAllLeaveStatus();
+		return ResponseEntity.ok(leaveStatus);
+	}
+	
+	@DeleteMapping("/{leaveStatusId}")
+	public ResponseEntity<String> deleteLeaveStatus(@PathVariable int leaveStatusId){
+		leaveStatusService.deleteLeaveStatus(leaveStatusId);
+		return ResponseEntity.ok("");
+	}
 }
--- src/main/java/com/erpm/leaveManagementService/services/LeaveStatusService.java
+++ src/main/java/com/erpm/leaveManagementService/services/LeaveStatusService.java
@@ -1,5 +1,38 @@
 package com.erpm.leaveManagementService.services;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.erpm.leaveManagementService.entitys.LeaveStatus;
+import com.erpm.leaveManagementService.repositorys.LeaveStatusRepository;
+
 public class LeaveStatusService {
+
+	@Autowired
+	LeaveStatusRepository leaveStatusRepository;
+
+	public LeaveStatus addLeaveStatus(LeaveStatus leaveStatus) {
+		return leaveStatusRepository.save(leaveStatus);
+	}
+
+	public LeaveStatus getLeaveStatusById(int leaveStatusId) {
+		LeaveStatus leaveStatus=null;
+		try {
+			leaveStatus = leaveStatusRepository.findById(leaveStatusId).get();
+		} catch (NoSuchElementException ex) {
+			System.out.println("there is no such element");
+		}
+		return leaveStatus;
+	}
+
+	public List<LeaveStatus> getAllLeaveStatus() {
+		return leaveStatusRepository.findAll();
+	}
+
+	public void deleteLeaveStatus(int leaveStatusId) {
+		leaveStatusRepository.deleteById(leaveStatusId);
+	}
 
 }
